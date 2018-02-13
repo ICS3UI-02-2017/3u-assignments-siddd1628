371ca468e684d647ad795a531cb9bfdac479ca20
siddd1628 <siddd1628@HRH-IC0027311.wrdsb.ca>
2018-02-13 11:58:26 -0500

    SDYGUL,

---
+++ src/Assignment1/newpackage/A1Q3.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment1.newpackage;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import java.awt.Color;
+
+/**
+ *
+ * @author siddd1628
+ */
+public class A1Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           //// Create a new city called daniTown
+      
+        City daniTown = new City();
+        
+        // Create a new robot to put into the city
+        RobotSE karel = new RobotSE(daniTown, 3, 0, Direction.EAST);
+        
+        // set the color of the robot
+        karel.setColor(Color.BLUE);
+        
+          // Create a new thing
+        new Thing(daniTown, 3, 1);
+        
+      //Create a wall
+        new Wall(daniTown, 3, 2, Direction.WEST);
+        
+         //Create a wall
+        new Wall(daniTown, 3, 2, Direction.NORTH);
+        
+        //Create a wall
+        new Wall(daniTown, 2, 3, Direction.WEST);
+        
+         //Create a wall
+        new Wall(daniTown, 1, 3, Direction.WEST);
+        
+           //Create a wall
+        new Wall(daniTown, 1, 3, Direction.NORTH);
+        
+         //Create a wall
+        new Wall(daniTown, 1, 3, Direction.EAST);
+        
+         //Create a wall
+        new Wall(daniTown, 2, 4, Direction.NORTH);
+        
+          //Create a wall
+        new Wall(daniTown, 2, 4, Direction.EAST);
+        
+         //Create a wall
+        new Wall(daniTown, 3, 4, Direction.EAST);
+        
+        
+    
+}}
