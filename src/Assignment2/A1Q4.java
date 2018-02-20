463fa6a321ae61a3714137c3892c83fb40365261
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-02-20 11:17:44 -0500

    second assignment

---
+++ src/Assignment2/A1Q4.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment2;
+
+import becker.robots.City;
+
+/**
+ *
+ * @author siddd1628
+ */
+public class A1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         //  Create a new city 
+         City dstown = new City();
+        
+        
+   
+    }
+    
+    
+}
