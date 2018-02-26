beb1f07ebb1d42e0c6924ebf08c713632607f2a8
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-02-26 10:26:22 -0500

    assignemnt2

--- src/Assignment2/A1Q4.java
+++ src/Assignment2/A1Q4.java
@@ -5,6 +5,8 @@
 package Assignment2;
 
 import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Wall;
 
 /**
  *
@@ -20,6 +22,27 @@ public class A1Q4 {
         
          //  Create a new city 
          City dstown = new City();
+        
+         
+       
+        new Wall(dstown, 1, 1, Direction.NORTH);
+        new Wall(dstown, 1, 1, Direction.EAST);
+        new Wall(dstown, 1, 1, Direction.WEST);
+        new Wall(dstown, 1, 1, Direction.SOUTH);
+        new Wall(dstown, 2, 2, Direction.WEST);
+        new Wall(dstown, 3, 2, Direction.WEST);
+        new Wall(dstown, 3, 2, Direction.SOUTH);
+        new Wall(dstown, 2, 2, Direction.NORTH);
+        new Wall(dstown, 2, 3, Direction.NORTH);
+        new Wall(dstown, 3, 3, Direction.EAST);
+        new Wall(dstown, 2, 3, Direction.EAST);
+        new Wall(dstown, 3, 3, Direction.SOUTH);
+        
+        
+        
+        
+        
+    
         
         
    
