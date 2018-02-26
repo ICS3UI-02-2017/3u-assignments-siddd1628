b3af78cc5c3287a4b13ef3ffd4f2194820d84455
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-02-26 11:18:19 -0500

    assignemnt3

---
+++ src/Assignment3/A1Q1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author siddd1628
+ */
+public class A1Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //  Create a new city 
+         City kw = new City();
+         
+           
+        // Create a new robot to put into the city
+        RobotSE karel = new RobotSE(kw, 0, 3, Direction.WEST);
+        
+        // Create a square
+        new Wall(kw, 1, 3, Direction.EAST);
+        new Wall(kw, 2, 3, Direction.EAST);
+        new Wall(kw, 1, 3, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        new Wall(kw, 2, 3, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.WEST);
+        new Wall(kw, 1, 2, Direction.WEST);
+        
+        // create an algorithm 
+  
+      
+    }
+}
