32052f6fa154b64e03642494c71d7495f9b60037
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-03-02 11:18:42 -0500

    AISSGNEMNT 4

---
+++ src/Assignment4/A4Q3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author siddd1628
+ */
+public class A4Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // use a SCANNER to read info
+            Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter two integers: ");
+
+String name = sc.nextLine() ;
+int a = sc.nextInt(); 
+int b = sc.nextInt();
+int c = sc.nextInt();
+int d = sc.nextInt();
+
+System.out.println("You input: " + a + " and " + b); 
+}
+    }
+
