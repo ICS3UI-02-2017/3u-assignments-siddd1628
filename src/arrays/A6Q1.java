cd638fc7a6a1e70551107275c3ed432759c37f20
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-04-12 11:17:49 -0400

    a4q4

--- src/arrays/A6Q1.java
+++ src/arrays/A6Q1.java
@@ -4,6 +4,8 @@
  */
 package arrays;
 
+import java.util.Scanner;
+
 /**
  *
  * @author siddd1628
@@ -15,6 +17,22 @@ public class A6Q1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+          // scanner for input
+        Scanner input = new Scanner(System.in);
+        
+        // make an array to store expenses
+        double[] expenses = new double[6];
+        // array to store expense names
+        String[] names = {"food", 
+                            "DJ", 
+                   "hall rental", 
+                   "decorations", 
+                         "staff",  
+                           "misc."};
+       
+        // ask for the number of students
+        System.out.println("How many students are in the class?");
         
     }
 }
