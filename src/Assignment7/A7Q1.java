e4cfe793570286845099f32337ccd87a6d5f525e
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-04-25 11:11:33 -0400

    assignment 7

---
+++ src/Assignment7/A7Q1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author siddd1628
+ */
+public class A7Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            // calculate the positive root of quadratic formula 
+    public static double [] circleArea (double a, double b, double c) {
+        // create the array to store both roots
+        double[] roots = new double [1];
+        //calcualte the roots
+        string user = in.nextLine();
+            double discrim = math.pi (user)
+                    math. pow(user,2)
+            roots[0] = (-b + Math.sqrt(discrim))/(2*a);
+            roots[1] = (-b + Math.sqrt(discrim))/(2*a);
+            //send back the root 
+            return roots;
+            
+        
+         
+    }
+}
