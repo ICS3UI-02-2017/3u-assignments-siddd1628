8b0eb7c201d08655619b3f3be30183584a8572af
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-04-27 11:17:10 -0400

    assignment 7

--- src/Assignment7/A7Q1.java
+++ src/Assignment7/A7Q1.java
@@ -18,17 +18,24 @@ public class A7Q1 {
     public static void main(String[] args) {
         // TODO code application logic here
             // calculate the positive root of quadratic formula 
-    public static double [] circleArea (double a, double b, double c) {
-        // create the array to store both roots
+    public static double  circleArea () {
+        
+        Scanner in = new Scanner (System.in);
+    }
+        // create the array to store bot roots
         double[] roots = new double [1];
         //calcualte the roots
-        string user = in.nextLine();
-            double discrim = math.pi (user)
+        System.out.println("What is the radius ?");
+        
+        double radius = input.nextDouble();
+        
+            double discrim = Math.PI * radius ; 
+                    double radiuus= dsicrim 
                     math. pow(user,2)
             roots[0] = (-b + Math.sqrt(discrim))/(2*a);
-            roots[1] = (-b + Math.sqrt(discrim))/(2*a);
+            
             //send back the root 
-            return roots;
+            return root;
             
         
          
