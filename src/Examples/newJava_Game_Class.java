3514381890bec6615d74cb5e7ad82820645fecda
siddd1628 <siddd1628@HRH-IC0027319.wrdsb.ca>
2018-05-14 11:16:53 -0400

    butsdfjs

---
+++ src/Examples/newJava_Game_Class.java
@@ -0,0 +1,208 @@
+package Examples;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author siddd1628
+ */
+public class newJava_Game_Class extends JComponent implements ActionListener {
+
+    // Height and Width of our game
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+
+    //Title of the window
+    String title = "My Game";
+
+    // sets the framerate and delay for our game
+    // this calculates the number of milliseconds per frame
+    // you just need to select an approproate framerate
+    int desiredFPS = 60;
+    int desiredTime = Math.round((1000 / desiredFPS));
+    
+    // timer used to run the game loop
+    // this is what keeps our time running smoothly :)
+    Timer gameTimer;
+
+    // YOUR GAME VARIABLES WOULD GO HERE
+    Color babypink = new Color(255, 214, 214);
+    int pacmanX = 100;
+    int pacmanAngle = 45;
+    int pacmanRotate = 270;
+    boolean pacmanClosing = false;
+
+        int mouseX = 0;
+        int mouseY = 0;
+
+    // GAME VARIABLES END HERE    
+
+    
+    // Constructor to create the Frame and place the panel in
+    // You will learn more about this in Grade 12 :)
+    public newJava_Game_Class(){
+        // creates a windows to show my game
+        JFrame frame = new JFrame(title);
+
+        // sets the size of my game
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        // adds the game to the window
+        frame.add(this);
+
+        // sets some options and size of the window automatically
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        // shows the window to the user
+        frame.setVisible(true);
+
+        // add listeners for keyboard and mouse
+        frame.addKeyListener(new Keyboard());
+        Mouse m = new Mouse();
+        this.addMouseMotionListener(m);
+        this.addMouseWheelListener(m);
+        this.addMouseListener(m);
+        
+        gameTimer = new Timer(desiredTime,this);
+        gameTimer.setRepeats(true);
+        gameTimer.start();
+    }
+
+    // drawing of the game happens in here
+    // we use the Graphics object, g, to perform the drawing
+    // NOTE: This is already double buffered!(helps with framerate/speed)
+    @Override
+    public void paintComponent(Graphics g) {
+        // always clear the screen first!
+     
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+        
+        
+        g.fillRect(0,0,WIDTH, HEIGHT); 
+        
+          
+
+        // GAME DRAWING GOES HERE
+           
+    
+	
+        
+       
+        
+		g.fillOval(100, 125, 155, 50);
+                
+               int[] triangleX = {mouseX,600,450};
+               int [] triangleY = {mouseY,500,500};
+               g.fillPolygon(triangleX,triangleY, 3);
+               
+               g.setColor(Color.YELLOW);
+               g.fillArc(pacmanX,75,100,100, pacmanAngle, pacmanRotate);
+              
+        // GAME DRAWING ENDS HERE
+    }
+
+    // This method is used to do any pre-setup you might need to do
+    // This is run before the game loop begins!
+    public void preSetup() {
+        // Any of your pre setup before the loop starts should go here
+
+    }
+
+    // The main game loop
+    // In here is where all the logic for my game will go
+    public void gameLoop() {
+        //move himmacross the screen
+      pacmanX = pacmanX + 3;  
+      //when pacman leaves the screen
+      if(pacmanX > WIDTH) {
+          pacmanX = - 100;
+      }
+      //make pacman roll
+      if(pacmanAngle <= 0){
+          pacmanClosing = false;
+      }
+      if (pacmanAngle >=45){
+          pacmanClosing = true;
+      }
+      if(pacmanClosing){
+      pacmanAngle = pacmanAngle - 1; 
+      pacmanRotate = pacmanRotate + 2; 
+    }else{ 
+    pacmanAngle = pacmanAngle + 1; 
+      pacmanRotate = pacmanRotate - 2;
+    }
+    }
+
+    // Used to implement any of the Mouse Actions
+    private class Mouse extends MouseAdapter {
+
+        // if a mouse button has been pressed down
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        // if a mouse button has been released
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        // if the scroll wheel has been moved
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+
+        }
+
+        // if the mouse has moved positions
+        @Override
+        public void mouseMoved(MouseEvent e) {
+               mouseX = e.getX();
+               mouseY = e.getY();
+        }
+    }
+
+    // Used to implements any of the Keyboard Actions
+    private class Keyboard extends KeyAdapter {
+
+        // if a key has been pressed down
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        // if a key has been released
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        preSetup();
+        gameLoop();
+        repaint();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creates an instance of my game
+        newJava_Game_Class game = new newJava_Game_Class();
+    }
+}
+
